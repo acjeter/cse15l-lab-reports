d654fe0d06bdf2d6247015f0be2553aa71a6ec39
Andrew Jeter <119645659+acjeter@users.noreply.github.com>
2023-04-22 14:04:05 -0700

    Initial StringServer Implementation
    Added Server.java from wavelet repo, and created StringServer.java

--- StringServer.java
+++ StringServer.java
@@ -1,1 +1,23 @@
+import java.io.IOException;
+import java.net.URI;
+// import java.util.ArrayList;
+
+class Handler implements URLHandler {
+    String runningString = "";
+    
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/")) {
+            return String.format(runningString);
+        }
+        else if (url.getPath().equals("/add-message?s=")) {
+            String[] urlStrings = url.getQuery().split("=");
+            runningString.join(urlStrings[1] + "\n");
+            System.out.println(runningString);
+            return String.format(runningString);
+        }
+        else {
+            return "Error 404 Not Found!";
+        }
+    }
+}
 
