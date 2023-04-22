8a155cf23b7b5d06e1a5e7978fda30a9af1da4ae
Andrew Jeter <119645659+acjeter@users.noreply.github.com>
2023-04-22 14:10:24 -0700

    Update StringServer.java
    Added main method for Server creation, in class StringServer.

--- StringServer.java
+++ StringServer.java
@@ -21,3 +21,15 @@ class Handler implements URLHandler {
     }
 }
 
+class StringServer {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0) {
+            System.out.println(
+                "Missing port number! Try any number between 1024 to 49151!");
+            return;
+        }
+        int port = Integer.parseInt(args[0]);
+        Server.start(port, new Handler());
+    }
+}
+
